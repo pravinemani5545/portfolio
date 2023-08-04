51a9e4cdb955f9d6982d4a8845bf23ec721c86bd
pravinemani5545 <pravinemanivannan8@gmail.com>
2023-08-04 17:04:01 -0400

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -17,6 +17,35 @@ import web3 from "../public/web3.png";
 import web4 from "../public/web4.png";
 import web5 from "../public/web5.png";
 import web6 from "../public/web6.png";
+import Link from "next/link"
+import SlideUp from "./SlideUp"
+import { BsGithub, BsArrowUpRightSquare } from "react-icons/bs"
+
+const projects = [
+  {
+    name: "UsePopcorn",
+    description:
+      "ThankfulThoughts is a web app that generates an appreciative sentence of something or someone you are thankful for.",
+    image: "/usePopcorn.png",
+    github: "https://github.com/pravinemani5545/usePopcorn",
+    link: "https://use-popcorn-pravinemani5545.vercel.app/",
+  },
+  {
+    name: "ClassyWeather",
+    description: "ClassyWeather will help you find the weather near you!",
+    image: "/classyWeather.png",
+    github: "https://github.com/pravinemani5545/classyWeather",
+    link: "https://classy-weather-pravinemani5545.vercel.app/",
+  },
+  {
+    name: "DormBuddy",
+    description:
+      "Find University Rental opportunities near you!",
+    image: "/dormBuddy.png",
+    github: "",
+    link: "",
+  },
+]
 
 export default function Home() {
   const [darkMode, setDarkMode] = useState(false);
@@ -130,6 +159,55 @@ export default function Home() {
               <p className="text-gray-800 py-1">Figma</p>
               <p className="text-gray-800 py-1">Indesign</p>
             </div>
+          </div>
+        </section>
+        <section id="projects">
+          <h1 className="my-10 text-center font-bold text-4xl">
+            Projects
+            <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
+          </h1>
+          <div className="flex flex-col space-y-28">
+            {projects.map((project, idx) => {
+              return (
+                <div key={idx}>
+                  <SlideUp offset="-300px 0px -300px 0px">
+                    <div className="flex flex-col  animate-slideUpCubiBezier animation-delay-2 md:flex-row md:space-x-12">
+                      <div className=" md:w-1/2">
+                        <Link href={project.link}>
+                          <Image
+                            src={project.image}
+                            alt=""
+                            width={1000}
+                            height={1000}
+                            className="rounded-xl shadow-xl hover:opacity-70"
+                          />
+                        </Link>
+                      </div>
+                      <div className="mt-8 md:w-1/2">
+                        <h1 className="text-4xl font-bold mb-6">{project.name}</h1>
+                        <p className="text-xl leading-7 mb-4 text-neutral-600 dark:text-neutral-400">
+                          {project.description}
+                        </p>
+                        <div className="flex flex-row align-bottom space-x-4">
+                          <Link href={project.github} target="_blank">
+                            <BsGithub
+                              size={30}
+                              className="hover:-translate-y-1 transition-transform cursor-pointer"
+                            />
+                          </Link>
+                          <Link href={project.link} target="_blank">
+                            <BsArrowUpRightSquare
+                              size={30}
+                              className="hover:-translate-y-1 transition-transform cursor-pointer"
+                            />
+                          </Link>
+                        </div>
+                      </div>
+                    </div>
+                  </SlideUp>
+                </div>
+              )
+            })}
           </div>
         </section>
         <section className="py-10">
