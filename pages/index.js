4059b63f24c51651b25cb686bcc6d1aebc84db73
pravinemani5545 <pravinemanivannan8@gmail.com>
2023-08-04 22:57:15 -0400

    Initial Commit

--- pages/index.js
+++ pages/index.js
@@ -174,7 +174,7 @@ export default function Home() {
           <div className='flex flex-col space-y-28'>
             {projects.map((project, idx) => {
               return (
-                <div key={idx}>
+                <div key={idx} className='mx-0 py-0 mb-0'>
                   <SlideUp offset='-300px 0px -300px 0px'>
                     <div className='flex flex-col  animate-slideUpCubiBezier animation-delay-2 md:flex-row md:space-x-12'>
                       <div className=' md:w-1/2'>
