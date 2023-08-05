1f27350d6b81d459a2d8d972ff4ec6f6b4b997b8
pravinemani5545 <pravinemanivannan8@gmail.com>
2023-08-04 23:13:27 -0400

    minor updates

--- pages/index.js
+++ pages/index.js
@@ -91,9 +91,9 @@ export default function Home() {
               Developer and designer.
             </h3>
             <p className='text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl'>
-              Your one-stop solution for programming and design needs, I'm
-              committed to exceeding your expectations. Let's team up below and
-              create something extraordinary together! 🚀
+              Your one-stop solution for programming and design needs, I&apos;m
+              committed to exceeding your expectations. Let&apos;s team up below
+              and create something extraordinary together! 🚀
             </p>
             <div className='text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400'>
               <AiFillTwitterCircle />
