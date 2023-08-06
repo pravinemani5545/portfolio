10c20453855d1186c7704c477065318044ba6c26
pravinemani5545 <pravinemanivannan8@gmail.com>
2023-08-06 01:54:29 -0400

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -17,6 +17,7 @@ import web3 from '../public/web3.png';
 import web4 from '../public/web4.png';
 import web5 from '../public/web5.png';
 import web6 from '../public/web6.png';
+import toronto from '../public/toronto.jpg';
 import Link from 'next/link';
 import SlideUp from './SlideUp';
 import { BsGithub, BsArrowUpRightSquare } from 'react-icons/bs';
@@ -105,42 +106,43 @@ export default function Home() {
             </div>
           </div>
         </section>
-        <section class='overflow-hidden bg-gray-50 my-12 rounded-2xl'>
-          <div class='mx-auto'>
-            <div class='grid items-center grid-cols-1 md:grid-cols-2'>
-              <div class='justify-start px-8 py-4'>
-                <h2 class='text-3xl font-bold leading-tight text-black sm:text-4xl lg:text-5xl'>
+        <section className='overflow-hidden bg-gray-50 my-12 rounded-2xl'>
+          <div className='mx-auto'>
+            <div className='grid items-center grid-cols-1 md:grid-cols-2'>
+              <div className='justify-start px-8 py-4'>
+                <h2 className='text-3xl font-bold leading-tight text-black sm:text-4xl lg:text-5xl'>
                   About me!
                 </h2>
-                <p class='max-w-2xl mt-3 text-xl leading-relaxed text-gray-600 md:mt-8'>
+                <p className='max-w-2xl mt-3 text-xl leading-relaxed text-gray-600 md:mt-8'>
                   Amet minim mollit non deserunt ullamco est sit aliqua dolor do
                   amet sint. Velit officia consequat duis enim velit mollit.
                   Exercitation veniam consequat sunt nostrud amet.
                 </p>
 
-                <p class='mt-4 text-xl text-gray-600 md:mt-8'>
-                  <span class='relative inline-block'>
-                    <span class='absolute inline-block w-full bottom-0.5 h-2 bg-yellow-300'></span>
-                    <span class='relative'> Have a question? </span>
+                <p className='mt-4 text-xl text-gray-600 md:mt-8'>
+                  <span className='relative inline-block'>
+                    <span className='absolute inline-block w-full bottom-0.5 h-2 bg-yellow-300'></span>
+                    <span className='relative'> Have a question? </span>
                   </span>
-                  <br class='block sm:hidden' />
+                  <br className='block sm:hidden' />
                   Ask me on{' '}
                   <a
                     href='#'
                     title=''
-                    class='transition-all duration-200 text-sky-500 hover:text-sky-600 hover:underline'
+                    className='transition-all duration-200 text-sky-500 hover:text-sky-600 hover:underline'
                   >
                     Twitter
                   </a>
                 </p>
               </div>
 
-              <div class='relative justify-end'>
-                <img
+              <div className='relative justify-end'>
+                <Image src={toronto} alt='' class='relative w-full xl:mx-auto 2xl:origin-bottom 2xl:scale-110 rounded-md'/>
+                {/* <img
                   class='relative w-full xl:mx-auto 2xl:origin-bottom 2xl:scale-110 rounded-md'
                   src='toronto.jpg'
                   alt=''
-                />
+                /> */}
               </div>
             </div>
           </div>
@@ -345,24 +347,24 @@ export default function Home() {
             </div>
           </div>
         </section> */}
-        <section class="bg-white dark:bg-gray-900">
-          <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-              <h2 class="mb-4 text-4xl tracking-tight font-bold text-center text-gray-900 dark:text-white">Let's build together</h2>
-              <p class="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Curious to learn more? Contact me below!</p>
-              <form action="#" class="space-y-8">
+        <section className="bg-white dark:bg-gray-900">
+          <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+              <h2 className="mb-4 text-4xl tracking-tight font-bold text-center text-gray-900 dark:text-white">Let&apos;s build together</h2>
+              <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Curious to learn more? Contact me below!</p>
+              <form action="#" className="space-y-8">
                   <div>
-                      <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
-                      <input type="email" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required />
+                      <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
+                      <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required />
                   </div>
                   <div>
-                      <label for="subject" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Subject</label>
-                      <input type="text" id="subject" class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Let us know how we can help you" required />
+                      <label for="subject" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Subject</label>
+                      <input type="text" id="subject" className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Let us know how we can help you" required />
                   </div>
                   <div class="sm:col-span-2">
-                      <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
-                      <textarea id="message" rows="6" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Leave a comment..."></textarea>
+                      <label for="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
+                      <textarea id="message" rows="6" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Leave a comment..."></textarea>
                   </div>
-                  <button type="submit" class="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send message</button>
+                  <button type="submit" className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send message</button>
               </form>
           </div>
         </section>
