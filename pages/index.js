69b451f54742951b37dff730ee9703c261f407fd
pravinemani5545 <pravinemanivannan8@gmail.com>
2023-08-06 21:27:06 -0400

    Initial Commit

--- pages/index.js
+++ pages/index.js
@@ -114,7 +114,12 @@ export default function Home() {
                   About me!
                 </h2>
                 <p className='max-w-2xl mt-3 text-xl leading-relaxed text-gray-600 md:mt-8'>
-                Hello! I&apos;m Pravine Manivannan, an enthusiastic and dedicated student with a passion for programming and technology. Currently pursuing my studies in Computer Engineering at McMaster University, I aspire to become a skilled developer and contribute to the ever-evolving world of technology.
+                  Hello! I&apos;m Pravine Manivannan, an enthusiastic and
+                  dedicated student with a passion for programming and
+                  technology. Currently pursuing my studies in Computer
+                  Engineering at McMaster University, I aspire to become a
+                  skilled developer and contribute to the ever-evolving world of
+                  technology.
                 </p>
 
                 <p className='mt-4 text-xl text-gray-600 md:mt-8'>
@@ -135,7 +140,11 @@ export default function Home() {
               </div>
 
               <div className='relative justify-end'>
-                <Image src={toronto} alt='' class='relative w-full xl:mx-auto 2xl:origin-bottom 2xl:scale-110 rounded-md'/>
+                <Image
+                  src={toronto}
+                  alt=''
+                  class='relative w-full xl:mx-auto 2xl:origin-bottom 2xl:scale-110 rounded-md'
+                />
                 {/* <img
                   class='relative w-full xl:mx-auto 2xl:origin-bottom 2xl:scale-110 rounded-md'
                   src='toronto.jpg'
@@ -147,9 +156,10 @@ export default function Home() {
         </section>
         <section>
           <div>
-            <h3 className='mt-12 font-bold text-4xl dark:text-white '>
-              Skills & Technologies:
-            </h3>
+            <h1 className='my-10 text-center font-bold text-4xl dark:text-white'>
+              Skills & Technologies
+              <hr className='w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded'></hr>
+            </h1>
             <p className='text-xl py-6 text-gray-800 dark:text-gray-200'>
               Since the beginning of my journey as a freelance designer and
               developer, I&apos;ve done remote work for
@@ -345,25 +355,69 @@ export default function Home() {
             </div>
           </div>
         </section> */}
-        <section className="bg-white dark:bg-gray-900">
-          <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-              <h2 className="mb-4 text-4xl tracking-tight font-bold text-center text-gray-900 dark:text-white">Let&apos;s build together</h2>
-              <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Curious to learn more? Contact me below!</p>
-              <form action="#" className="space-y-8">
-                  <div>
-                      <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
-                      <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required />
-                  </div>
-                  <div>
-                      <label for="subject" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Subject</label>
-                      <input type="text" id="subject" className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Let us know how we can help you" required />
-                  </div>
-                  <div class="sm:col-span-2">
-                      <label for="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
-                      <textarea id="message" rows="6" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Leave a comment..."></textarea>
-                  </div>
-                  <button type="submit" className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send message</button>
-              </form>
+        <section className='bg-white dark:bg-gray-900'>
+          <div className='py-8 lg:py-16 px-4 mx-auto max-w-screen-md'>
+            <h1 className='my-10 text-center font-bold text-4xl dark:text-white'>
+              Let&apos;s build together
+              <hr className='w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded'></hr>
+            </h1>
+            <p className='mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl'>
+              Curious to learn more? Contact me below!
+            </p>
+            <form action='#' className='space-y-8'>
+              <div>
+                <label
+                  for='email'
+                  className='block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300'
+                >
+                  Your email
+                </label>
+                <input
+                  type='email'
+                  id='email'
+                  className='shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light'
+                  placeholder='name@flowbite.com'
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  for='subject'
+                  className='block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300'
+                >
+                  Subject
+                </label>
+                <input
+                  type='text'
+                  id='subject'
+                  className='block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light'
+                  placeholder='Let us know how we can help you'
+                  required
+                />
+              </div>
+              <div class='sm:col-span-2'>
+                <label
+                  for='message'
+                  className='block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400'
+                >
+                  Your message
+                </label>
+                <textarea
+                  id='message'
+                  rows='6'
+                  className='block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500'
+                  placeholder='Leave a comment...'
+                ></textarea>
+              </div>
+              <div className='text-center'>
+                <button
+                  type='submit'
+                  className='py-3 px-5 text-sm font-medium bg-blue-700 text-white rounded-lg  focus:ring-4 focus:outline-none focus:bg-blue-700 dark:accent-white dark:hover:text-white dark:focus:ring-white'
+                >
+                  Send message
+                </button>
+              </div>
+            </form>
           </div>
         </section>
       </main>
