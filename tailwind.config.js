5bc9dffb134af0ebd83e778d446cadbf7156baf0
pravinemani5545 <pravinemanivannan8@gmail.com>
2023-08-08 00:19:38 -0400

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,13 @@ module.exports = {
         poppins: ["Poppins", "sans-serif"],
       },
     },
+    // screens: {
+    //   'sm': '640px',
+    //   'md': '768px',
+    //   'lg': '1024px',
+    //   'xl': '1280px',
+    //   '2xl': '1800px',
+    //   }
   },
   plugins: [],
 };
